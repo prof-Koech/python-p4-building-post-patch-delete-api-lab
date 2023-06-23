9fb2ac140d18bd4d4d349653eca2781b7253ad83
Kennedy <kiplangatkennedy019@gmail.com>
2023-06-23 12:25:23 +0300

    completed the assignment

--- server/app.py
+++ server/app.py
@@ -66,5 +66,116 @@ def most_expensive_baked_good():
     )
     return response
 
+@app.route('/baked_goods', methods=["GET", "POST", "DELETE"])
+def baked_goods():
+    if request.method == "GET":
+        all_cakes = []
+        cakes = BakedGood.query.all()
+        for cake in cakes:
+            cake_dict = cake.to_dict()
+            all_cakes.append(cake_dict)
+
+        response1 = make_response(jsonify(all_cakes), 200)
+        
+        
+        return response1
+
+    elif request.method == "POST":
+        new_baked = BakedGood(
+            name=request.form.get("name"),
+            price=request.form.get("price"),
+            created_at=request.form.get("created_at"),
+            updated_at=request.form.get("updated_at"),
+            bakery_id=request.form.get("bakery_id")
+        )
+        db.session.add(new_baked)
+        db.session.commit()
+
+        review_dict = new_baked.to_dict()
+        response2 = make_response(jsonify(review_dict), 201)  # Update the status code to 201
+        response2.headers["Content-Type"] = "application/json"
+        
+        
+        return response2
+
+@app.route('/baked_goods/<int:id>',methods=["GET","PATCH","DELETE",])
+def delete_baked_goods(id):
+    single_baked_good=BakedGood.query.filter_by(id=id).first()
+    if request.method=="GET":
+        single_baked_good_dict=single_baked_good.to_dict()
+        response1=make_response(jsonify(single_baked_good_dict),200)
+        response1.headers["Content-Type"]="application/json"
+       
+       
+        return response1
+    
+    elif request.method=="PATCH":
+        for attribute in request.form:
+            setattr(attribute,single_baked_good,request.form.get(attribute))
+        db.session.commit()
+        single_baked_good_dict2=single_baked_good.to_dict()
+        response3=make_response(jsonify(d=single_baked_good_dict2),200)
+        response3.headers["Content-Type"]="application/json"
+       
+       
+        return response3
+
+
+    elif request.method=="DELETE":
+        db.session.delete(single_baked_good)
+        db.session.commit()
+
+        response_body = {
+                "delete_successful": True,
+                "message": "Review deleted."
+            }
+        response2=make_response(jsonify(response_body),200)
+        response2.headers["Content-Type"]="application/json"
+       
+       
+        return response2
+    
+        
+    
+@app.route('/bakeries/<int:id>',methods=["GET","PATCH","DELETE"])
+def bakeries_id(id):
+    single_bakery=Bakery.query.filter_by(id=id).first()
+
+    if request.method=="GET":
+        single_bakery_dict=single_bakery.to_dict()
+        response1=make_response(jsonify(single_bakery_dict),200)
+        response1.headers["Content-Type"]="application/json"
+        
+        
+        return response1
+    
+
+    elif request.method=="PATCH":
+        for attribute in request.form:
+            setattr(single_bakery,attribute,request.form.get(attribute))
+        db.session.commit()
+        single_bakery_dict2=single_bakery.to_dict()
+        response2=make_response(jsonify(single_bakery_dict2),200)
+        response2.headers["Content-Type"]="application/json"
+       
+       
+        return response2
+    
+    elif request.method=="DELETE":
+        db.session.delete(single_bakery)
+        db.session.commit()
+
+        response_body = {
+                "delete_successful": True,
+                "message": "Review deleted."
+            }
+        response3=make_response(jsonify(response_body),200)
+        response3.headers["Content-Type"]="application/json"
+        
+        
+        return response3
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
